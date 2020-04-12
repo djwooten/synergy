Changed to natural log scale, and fixed musyc jacobian

synergy/combination/musyc.py
@@ -48,23 +48,23 @@ class MuSyC:
         self.converged = False
 
         with np.errstate(divide='ignore'):
-            self.logh1_bounds = (np.log10(h1_bounds[0]), np.log10(h1_bounds[1]))
-            self.logC1_bounds = (np.log10(C1_bounds[0]), np.log10(C1_bounds[1]))
-            self.logh2_bounds = (np.log10(h2_bounds[0]), np.log10(h2_bounds[1]))
-            self.logC2_bounds = (np.log10(C2_bounds[0]), np.log10(C2_bounds[1]))
+            self.logh1_bounds = (np.log(h1_bounds[0]), np.log(h1_bounds[1]))
+            self.logC1_bounds = (np.log(C1_bounds[0]), np.log(C1_bounds[1]))
+            self.logh2_bounds = (np.log(h2_bounds[0]), np.log(h2_bounds[1]))
+            self.logC2_bounds = (np.log(C2_bounds[0]), np.log(C2_bounds[1]))
             
-            self.logalpha12_bounds = (np.log10(alpha12_bounds[0]), np.log10(alpha12_bounds[1]))
-            self.logalpha21_bounds = (np.log10(alpha21_bounds[0]), np.log10(alpha21_bounds[1]))
+            self.logalpha12_bounds = (np.log(alpha12_bounds[0]), np.log(alpha12_bounds[1]))
+            self.logalpha21_bounds = (np.log(alpha21_bounds[0]), np.log(alpha21_bounds[1]))
 
-            self.loggamma12_bounds = (np.log10(gamma12_bounds[0]), np.log10(gamma12_bounds[1]))
-            self.loggamma21_bounds = (np.log10(gamma21_bounds[0]), np.log10(gamma21_bounds[1]))
+            self.loggamma12_bounds = (np.log(gamma12_bounds[0]), np.log(gamma12_bounds[1]))
+            self.loggamma21_bounds = (np.log(gamma21_bounds[0]), np.log(gamma21_bounds[1]))
 
     def fit(self, d1, d2, E, drug1_model=None, drug2_model=None, use_jacobian = True, p0=None, **kwargs):
         """
         """
-        #f = lambda d, E0, E1, E2, E3, logh1, logh2, logC1, logC2, logalpha12, logalpha21, loggamma12, loggamma21: self._model(d[0], d[1], E0, E1, E2, E3, 10.**logh1, 10.**logh2, 10.**logC1, 10.**logC2, self.r1, self.r2, 10.**logalpha12, 10.**logalpha21, 10.**loggamma12, 10.**loggamma21)
+        #f = lambda d, E0, E1, E2, E3, logh1, logh2, logC1, logC2, logalpha12, logalpha21, loggamma12, loggamma21: self._model(d[0], d[1], E0, E1, E2, E3, np.exp(logh1), np.exp(logh2), np.exp(logC1), np.exp(logC2), self.r1, self.r2, np.exp(logalpha12), np.exp(logalpha21), np.exp(loggamma12), np.exp(loggamma21))
 
-        f = lambda d, E0, E1, E2, E3, logh1, logh2, logC1, logC2, logalpha12, logalpha21: self._model(d[0], d[1], E0, E1, E2, E3, 10.**logh1, 10.**logh2, 10.**logC1, 10.**logC2, self.r1, self.r2, 10.**logalpha12, 10.**logalpha21)
+        f = lambda d, E0, E1, E2, E3, logh1, logh2, logC1, logC2, logalpha12, logalpha21: self._model(d[0], d[1], E0, E1, E2, E3, np.exp(logh1), np.exp(logh2), np.exp(logC1), np.exp(logC2), self.r1, self.r2, np.exp(logalpha12), np.exp(logalpha21))
 
         jacobian = lambda d, E0, E1, E2, E3, logh1, logh2, logC1, logC2, logalpha12, logalpha21: musyc_jacobian.jacobian(d[0], d[1], E0, E1, E2, E3, logh1, logh2, logC1, logC2, self.r1, self.r2, logalpha12, logalpha21)
 
@@ -91,12 +91,12 @@ class MuSyC:
         self.E1 = E1
         self.E2 = E2
         self.E3 = E3
-        self.h1 = 10.**logh1
-        self.h2 = 10.**logh2
-        self.C1 = 10.**logC1
-        self.C2 = 10.**logC2
-        self.alpha12 = 10.**logalpha12
-        self.alpha21 = 10.**logalpha21
+        self.h1 = np.exp(logh1)
+        self.h2 = np.exp(logh2)
+        self.C1 = np.exp(logC1)
+        self.C2 = np.exp(logC2)
+        self.alpha12 = np.exp(logalpha12)
+        self.alpha21 = np.exp(logalpha21)
         self.beta = (min(E1,E2)-E3) / (E0 - min(E1,E2))
         self.gamma12 = 1.
         self.gamma21 = 1.
@@ -107,7 +107,7 @@ class MuSyC:
             p0 = list(p0)
             # p0 = (E0, E1, E2, E3, h1, h2, C1, C2, alpha12, alpha21, gamma12, gamma21) - starting from h1 to the end, all guesses must be log-transformed
             for i in range(4,len(p0)):
-                p0[i] = np.log10(p0[i])
+                p0[i] = np.log(p0[i])
             utils.sanitize_initial_guess(p0, bounds)
             return p0
         else:
@@ -129,7 +129,7 @@ class MuSyC:
             # Could infer orientation from sign of Y=(E0-E1)+(E0-E2). Y>0 means drug causes E to decrease. Y<0 means drug causes E to increase. Or let user set it
             else: E3 = np.min(E)
             
-            p0 = [(E0_1+E0_2)/2., E1, E2, E3, np.log10(h1), np.log10(h2), np.log10(C1), np.log10(C2), 0, 0] #, 0, 0] # For gamma
+            p0 = [(E0_1+E0_2)/2., E1, E2, E3, np.log(h1), np.log(h2), np.log(C1), np.log(C2), 0, 0] #, 0, 0] # For gamma
             
             utils.sanitize_initial_guess(p0, bounds)
 
@@ -139,12 +139,6 @@ class MuSyC:
         if not self._is_parameterized():
             raise ModelNotParameterizedError()
         return self._model(d1, d2, self.E0, self.E1, self.E2, self.E3, self.h1, self.h2, self.C1, self.C2, self.r1, self.r2, self.alpha12, self.alpha21)
-
-    def _modelgamma(self, d1, d2, E0, E1, E2, E3, h1, h2, r1, r1r, r2, r2r, alpha12, alpha21, gamma12, gamma21):
-        #U, A1, A2, A12 = self._getUA(d1, d2, h1, h2, alpha12, alpha21, r1, r1r, r2, r2r)
-
-        #return U*E0 + A1*E1 + A2*E2 + A12*E3
-        return 0
 
     def _is_parameterized(self):
         return None not in (self.E0, self.E1, self.E2, self.E3, self.h1, self.h2, self.C1, self.C2, self.alpha12, self.alpha21, self.gamma12, self.gamma21, self.r1, self.r2)

synergy/single/hill.py
@@ -17,25 +17,25 @@ class Hill:
         self.h_bounds=h_bounds
         self.C_bounds=C_bounds
         with np.errstate(divide='ignore'):
-            self.logh_bounds = (np.log10(h_bounds[0]), np.log10(h_bounds[1]))
-            self.logC_bounds = (np.log10(C_bounds[0]), np.log10(C_bounds[1]))
+            self.logh_bounds = (np.log(h_bounds[0]), np.log(h_bounds[1]))
+            self.logC_bounds = (np.log(C_bounds[0]), np.log(C_bounds[1]))
 
         self.jacobian_queue = multiprocessing.Queue()
         self.converged = False
     
     def fit(self, d, E, use_jacobian=True, **kwargs):
-        f = lambda d, E0, E1, logh, logC: self._model(d, E0, E1, 10.**logh, 10.**logC)
+        f = lambda d, E0, E1, logh, logC: self._model(d, E0, E1, np.exp(logh), np.exp(logC))
 
         bounds = tuple(zip(self.E0_bounds, self.Emax_bounds, self.logh_bounds, self.logC_bounds))
 
         if 'p0' in kwargs:
             p0 = list(kwargs.get('p0'))
-            p0[2] = np.log10(p0[2])
-            p0[3] = np.log10(p0[3])
+            p0[2] = np.log(p0[2])
+            p0[3] = np.log(p0[3])
             utils.sanitize_initial_guess(p0, bounds)
             kwargs['p0'] = p0
         else:
-            p0 = [max(E), min(E), 0, np.log10(np.median(d))]
+            p0 = [max(E), min(E), 0, np.log(np.median(d))]
             utils.sanitize_initial_guess(p0, bounds)
             kwargs['p0'] = p0
 
@@ -51,32 +51,34 @@ class Hill:
             E0 = np.max(E)
             E1 = np.min(E)
             logh = 0
-            logC = np.log10(np.median(d))
+            logC = np.log(np.median(d))
             self.converged = False
 
         
         
         self.E0 = E0
         self.Emax = E1
-        self.h = 10.**logh
-        self.C = 10.**logC
+        self.h = np.exp(logh)
+        self.C = np.exp(logC)
 
+    # TODO: Implement Jacobian
+    # TODO: Separate classes Hill2P() and Hill4P()
     def fit_2parameter(self, d, E, use_jacobian=True, **kwargs):
         if self.E0 is None: self.E0 = 1.
         if self.Emax is None: self.Emax = 0.
 
-        f = lambda d, logh, logC: self._model(d, self.E0, self.Emax, 10.**logh, 10.**logC)
+        f = lambda d, logh, logC: self._model(d, self.E0, self.Emax, np.exp(logh), np.exp(logC))
 
         bounds = tuple(zip(self.logh_bounds, self.logC_bounds))
 
         if 'p0' in kwargs:
             p0 = list(kwargs.get('p0'))
-            p0[0] = np.log10(p0[0])
-            p0[1] = np.log10(p0[1])
+            p0[0] = np.log(p0[0])
+            p0[1] = np.log(p0[1])
             kwargs['p0'] = p0
             utils.sanitize_initial_guess(p0, bounds)
         else:
-            p0 = [0, np.log10(np.median(d))]
+            p0 = [0, np.log(np.median(d))]
             utils.sanitize_initial_guess(p0, bounds)
             kwargs['p0'] = p0
 
@@ -84,8 +86,8 @@ class Hill:
 
         popt1, pcov = curve_fit(f, d, E, bounds=bounds, **kwargs)
         logh, logC = popt1
-        self.h = 10.**logh
-        self.C = 10.**logC
+        self.h = np.exp(logh)
+        self.C = np.exp(logC)
 
 
     def fit_CI(self, d, E):
@@ -112,60 +114,19 @@ class Hill:
         d = np.float_power((E0-Emax)/(E-Emax)-1.,1./h)*C
         return d
 
-    def _model_jacobian_linear(self, d, E0, Emax, h, C):
-        dh = d**h
-        Ch = C**h
-        logd = np.log(d)
-        logC = np.log(C)
-
-        jE0 = 1 - dh/(Ch+dh)
-        jEmax = 1-jE0
-        jC = (Emax-E0)*dh *(h*C**(h-1)) / (Ch+dh)**2
-        jh = (Emax-E0) * ((Ch+dh)*logd*dh - dh*(logC*Ch + logd*dh)) / (Ch+dh)**2
-        return np.asarray([jE0, jEmax, jh, jC]).transpose()
-
     def _model_jacobian(self, d, E0, Emax, logh, logC):
-        dh = d**(10.**logh)
-        Ch = (10.**logC)**(10.**logh)
+        dh = d**(np.exp(logh))
+        Ch = (np.exp(logC))**(np.exp(logh))
         logd = np.log(d)
-        ln10 = np.log(10)
 
         jE0 = 1 - dh/(Ch+dh)
         jEmax = 1-jE0
 
-        jC = (E0-Emax)*dh*ln10*10.**(logh+logC)*(10.**logC)**(10.**logh-1) / ((Ch+dh)*(Ch+dh))
+        jC = (E0-Emax)*dh*np.exp(logh+logC)*(np.exp(logC))**(np.exp(logh)-1) / ((Ch+dh)*(Ch+dh))
 
-        jh = (Emax-E0)*dh*10.**logh*ln10 * ((Ch+dh)*logd - (logC*ln10*Ch + logd*dh)) / ((Ch+dh)*(Ch+dh))
+        jh = (Emax-E0)*dh*np.exp(logh) * ((Ch+dh)*logd - (logC*Ch + logd*dh)) / ((Ch+dh)*(Ch+dh))
         
         return np.hstack((jE0.reshape(-1,1), jEmax.reshape(-1,1), jh.reshape(-1,1), jC.reshape(-1,1)))
-
-    @DeprecationWarning
-    def _model_jacobian_parallel(self, d, E0, Emax, h, C):
-        dh = d**(10.**h)
-        Ch = (10.**C)**(10.**h)
-        logd = np.log(d)
-        ln10 = np.log(10)
-
-        jobs = [multiprocessing.Process(target=func, args=(self.jacobian_queue, d, E0, Emax, h, C, dh, Ch, logd, ln10)) for func in (self._model_jacobian_E0, self._model_jacobian_Emax, self._model_jacobian_h, self._model_jacobian_C)]
-        for job in jobs: job.start()
-        for job in jobs: job.join()
-        return np.hstack([self.jacobian_queue.get().reshape(-1,1) for job in jobs])
-
-    @DeprecationWarning
-    def _model_jacobian_E0(self, queue, d, E0, Emax, h, C, dh, Ch, logd, ln10):
-        queue.put(1 - dh/(Ch+dh))
-
-    @DeprecationWarning
-    def _model_jacobian_Emax(self, queue, d, E0, Emax, h, C, dh, Ch, logd, ln10):
-        queue.put(dh/(Ch+dh))
-
-    @DeprecationWarning
-    def _model_jacobian_h(self, queue, d, E0, Emax, h, C, dh, Ch, logd, ln10):
-        queue.put((Emax-E0)*dh*10.**h*ln10 * ((Ch+dh)*logd - (C*ln10*Ch + logd*dh)) / ((Ch+dh)*(Ch+dh)))
-
-    @DeprecationWarning
-    def _model_jacobian_C(self, queue, d, E0, Emax, h, C, dh, Ch, logd, ln10):
-        queue.put((E0-Emax)*dh*ln10*10.**(h+C)*(10.**C)**(10.**h-1) / ((Ch+dh)*(Ch+dh)))
 
     def _is_parameterized(self):
         return None not in (self.E0, self.Emax, self.h, self.C)

synergy/tests/test_hill_jac.py
@@ -70,9 +70,9 @@ print("Not using")
 
 
 # Numerically check gradient
-logh = np.log10(h)
-logC = np.log10(C)
-f = lambda logC: model._model(C*3, E0, Emax, 10.**logh, 10.**logC)
+logh = np.log(h)
+logC = np.log(C)
+f = lambda logC: model._model(C*3, E0, Emax, np.exp(logh), np.exp(logC))
 jac =  lambda logC: model._model_jacobian(C*3, E0, Emax, logh, logC)[:,3]
 
 check_grad(f, jac, np.asarray([0]))

synergy/tests/test_musyc.py
@@ -4,49 +4,50 @@ import synergy.combination.musyc as musyc
 import synergy.combination.musyc_jacobian as mj
 
 
-E0, E1, E2, E3 = 1, 0.3, 0.1, 0.4
+E0, E1, E2, E3 = 1, 0.2, 0.1, 0.4
 h1, h2 = 2.3, 0.8
 C1, C2 = 1e-2, 1e-1
-alpha12, alpha21 = 3.2, 0.1
+alpha12, alpha21 = 3.2, 1.1
 
-model = musyc.MuSyC(E0=E0, E1=E1, E2=E2, E3=E3, h1=h1, h2=h2, C1=C1, C2=C2, alpha12=alpha12, alpha21=alpha21)
+model = musyc.MuSyC(E0=E0, E1=E1, E2=E2, E3=E3, h1=h1, h2=h2, C1=C1, C2=C2, alpha12=alpha12, alpha21=alpha21, r1=100., r2=100.)
 
 r1 = model.r1
 r2 = model.r2
 
-npoints = 20
+npoints = 8
 
 d1 = np.logspace(-3,0,num=npoints)
-d2 = np.logspace(-2,0,num=npoints)
+d2 = np.logspace(-2,1,num=npoints)
 D1, D2 = np.meshgrid(d1,d2)
 D1 = D1.flatten()
 D2 = D2.flatten()
 
 
 E = model._model(D1, D2, E0, E1, E2, E3, h1, h2, C1, C2, r1, r2, alpha12, alpha21)
-Efit = E*(1+(np.random.rand(len(D1))-0.5)/50.)
+Efit = E*(1+(np.random.rand(len(D1))-0.5)/5.)
 
 model.fit(D1, D2, Efit)
 #%timeit model.fit(D1, D2, Efit)
+#%timeit model.fit(D1, D2, Efit, use_jacobian=False)
 # With Jacobian
 # noise /5.
-# 31.5 ms ± 213 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
-# 52.5 ms ± 202 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
-# 22.4 ms ± 366 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
+# 73.5 ms ± 965 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
+# 63.7 ms ± 203 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
 
-# Without Jacobian
+# Without Jacobian (frequently has "covariance of parameters" warning)
 # noise /5.
+# 26.1 ms ± 385 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
+# 21.5 ms ± 422 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
+# 29.6 ms ± 101 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
+# 30.3 ms ± 284 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
 # 20 ms ± 137 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
 # 25.2 ms ± 343 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
 # 25.8 ms ± 302 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
 #%timeit model.fit(D1, D2, Efit, use_jacobian=False)
 
-
-jac = mj.jacobian(D1, D2, E0, E1, E2, E3, np.log10(h1), np.log10(h2), np.log10(C1), np.log10(C2), 100., 100., np.log10(alpha12), np.log10(alpha21))
-
-
-
 print(model)
+#model.fit(D1, D2, Efit, use_jacobian=False)
+#print(model)
 
 fig = plt.figure(figsize=(8,3))
 

synergy/tests/test_musyc_jac.py
@@ -20,25 +20,30 @@ r2 = model.r2
 d = np.logspace(-3,1,num=30)
 
 # Numerically check gradient
-logh1 = np.log10(h1)
-logC1 = np.log10(C1)
-logh2 = np.log10(h2)
-logC2 = np.log10(C2)
-logalpha12 = np.log10(alpha12)
-logalpha21 = np.log10(alpha21)
+logh1 = np.log(h1)
+logC1 = np.log(C1)
+logh2 = np.log(h2)
+logC2 = np.log(C2)
+logalpha12 = np.log(alpha12)
+logalpha21 = np.log(alpha21)
 
 # Jacobian return order
 # E0, E1, E2, E3, logh1, logh2, logC1, logC2, logalpha12, logalpha21
-f = lambda logalpha12: model._model(C1*2., C2/2., E0, E1, E2, E3, 10.**logh1, 10.**logh2, 10.**logC1, 10.**logC2, r1, r2, 10.**logalpha12, 10.**logalpha21)
-
-jac =  lambda logalpha12: musyc_jacobian.jacobian(C1*2., C2/2., E0, E1, E2, E3, 10.**logh1, 10.**logh2, 10.**logC1, 10.**logC2, r1, r2, 10.**logalpha12, 10.**logalpha21)
-
-check_grad(f, jac, np.asarray([-2]))
+f = lambda logalpha21: model._model(C1*2., C2/2., E0, E1, E2, E3, np.exp(logh1), np.exp(logh2), np.exp(logC1), np.exp(logC2), r1, r2, np.exp(logalpha12), np.exp(logalpha21))
 
 fj = lambda i: approx_fprime(np.asarray([i]), f, 0.0001)
-npfv = np.vectorize(fj)
-npfv(np.asarray([-1,0,1]))
+print(fj(logalpha21))
 
-jac(np.asarray([-1,0,1]))
+jac =  lambda E0: musyc_jacobian.jacobian(C1*2., C2/2., E0, E1, E2, E3, logh1, logh2, logC1, logC2, r1, r2, logalpha12, logalpha21)
 
-# npfv and jac should return the same values - CHECK musyc_jacobian - especially for alpha...
+#       jac                                 fj
+#  2.23939104e-01, #    E0              0.2239391 # These are all correct
+#  2.63567871e-01, #    E1              0.26356787 # Also, it makes sense they +
+#  1.30952694e-01, #    E2              0.13095269
+#  3.81540331e-01, #    E3              0.38154033
+#  1.76168986e-02,      logh1           -0.12307921 # These could be + or (-)
+# -2.82330684e-02,      logh2           -0.04041386 # - makes tentative sense
+# -7.19772057e-02,      logC1           0.18284021 # These SHOULD be positive
+#  4.10819668e-01,      logC2           0.08579638
+#  7.54493621e-02,      logalpha12      -0.08728012 # These SHOULD be negative
+#  3.00830660e-04])     logalpha21      -0.00333315