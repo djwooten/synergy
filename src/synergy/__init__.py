0e5090830e2047d8b893df6ca255c113ad261dc6
David Wooten <dwooten@psu.edu>
2021-06-13 20:44:59 -0600

    Bumping version number

--- src/synergy/__init__.py
+++ src/synergy/__init__.py
@@ -1,4 +1,4 @@
-__version__="0.4.7"
+__version__="0.5.0"
 __author__="David J. Wooten"
 __citation__="Wooten, DJ and Albert, RA. synergy - A Python library for calculating drug combination synergy. (2020) Bioinformatics. https://doi.org/10.1093/bioinformatics/btaa826"
 
